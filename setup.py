608001d9579a1f74d13f2f8c803f4b6680d4d213
Rani <ranihorev@gmail.com>
2018-03-05 15:33:41 -0800

    Add null pool

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sharelock_utils',
-      version='0.3.11',
+      version='0.3.12',
       description='Simple utils for Sharelock',
       url='',
       author='Rani',
--- sharelock_utils/dbUtils.py
+++ sharelock_utils/dbUtils.py
@@ -2,17 +2,21 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.pool import NullPool
 
 
-def get_db_conn():
+def get_db_conn(null_pool=False):
     host = os.environ.get('db_host')
     name = os.environ.get('db_name')
     user = os.environ.get('db_user')
     password = os.environ.get('db_password')
-    engine = create_engine(
-        'postgresql://{user}:{password}@{host}/{db}'.format(user=user, password=password, host=host, db=name)
-    )
+    full_path = 'postgresql://{user}:{password}@{host}/{db}'.format(user=user, password=password, host=host, db=name)
+    if null_pool:
+        engine = create_engine(full_path, poolclass=NullPool)
+    else:
+        engine = create_engine(full_path)
     return engine
+
 
 def get_engine_and_base():
     engine = get_db_conn()
