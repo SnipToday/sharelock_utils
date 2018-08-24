3b190f0e7cb3c68bb84935047251417ff0d59e5c
Rani <ranihorev@gmail.com>
2018-08-23 17:45:57 -0700

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sharelock_utils',
-      version='0.3.15',
+      version='0.3.16',
       description='Simple utils for Sharelock',
       url='',
       author='Rani',
